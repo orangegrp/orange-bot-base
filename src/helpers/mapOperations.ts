9523b25226f1a912b0d365e7f9fadc23247ea12d
tpslr <topias.lyyra@gmail.com>
2024-09-18 15:32:48 +0300

    added helper find for maps

--- src/helpers/mapOperations.ts
+++ src/helpers/mapOperations.ts
@@ -4,6 +4,12 @@ function some<K, V>(it: Map<K, V>, pred: (k: K, v: V) => boolean) {
             return true;
     return false;
 }
+function find<K, V>(it: Map<K, V>, pred: (key: K, value: V) => boolean): { key: K, value: V } | undefined {
+    for (const [key, value] of it) {
+        if (pred(key, value)) return { key, value };
+    }
+    return undefined;
+}
 
 
-export default { some }
+export default { some, find }
